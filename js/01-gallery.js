44bfbf36ce9d60f0329f3f5d04e537318ffa3e9f
MYKOLA SHEREMET <artem.vazovskin@gmail.com>
2022-07-17 17:25:32 +0300

    finised task 2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,41 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const divOfGallery = document.querySelector('.gallery');
+
+const renderItemsGallery = (pictures) => galleryItems.map(({ original, preview, description }) =>
+  `<div class="gallery__item">
+      <a class="gallery__link" href="${original}">
+      <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"/>
+      </a>
+      </div>`).join("");
+
+divOfGallery.insertAdjacentHTML('afterbegin', renderItemsGallery(galleryItems));
+
+divOfGallery.addEventListener('click', bigImage);
+
+function bigImage(e) {
+  e.preventDefault();
+  if (e.target.nodeName !== 'IMG') {
+    return
+  };
+  
+  const instance = basicLightbox.create(
+    `<img src="${e.target.dataset.source}"  alt="${e.target.description}"/>`,
+  );
+  instance.show()
+  
+  window.addEventListener('keydown', closePressEsp);
+
+  function closePressEsp(e) {
+    if (e.code === "Escape") {
+  instance.close()   
+    }
+  }
+  
+}
+
